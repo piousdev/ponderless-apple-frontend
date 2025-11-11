84c954ae03297eb3ffcfb6479fdb325c5c290d87
piousalpha <piousalpha@gmail.com>
2025-11-11 21:17:30 +0100

    Initial Commit

---
+++ Ponderless/PonderlessApp.swift
@@ -0,0 +1,17 @@
+//
+//  PonderlessApp.swift
+//  Ponderless
+//
+//  Created by Pious Alpha on 11/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct PonderlessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PonderlessTests/PonderlessTests.swift
@@ -0,0 +1,17 @@
+//
+//  PonderlessTests.swift
+//  PonderlessTests
+//
+//  Created by Pious Alpha on 11/11/2025.
+//
+
+import Testing
+@testable import Ponderless
+
+struct PonderlessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
